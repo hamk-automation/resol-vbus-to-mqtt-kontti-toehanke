3db69fde22bce2e48b0134b144e8f34b19227d79
Khoa Dang <khoa.dang@hamk.fi>
2019-04-30 13:24:03 +0100

    working state on pi

--- tableParse.js
+++
@@ -1,8 +0,0 @@
-const tabletojson = require('tabletojson');
-const fs = require('fs');
-const path = require('path');
-
-const html = fs.readFileSync('table1.html', { encoding: 'UTF-8' });
-const converted = tabletojson.convert(html);
-fs.writeFileSync('parsedTable.json', JSON.stringify(converted))
-console.log(converted);
